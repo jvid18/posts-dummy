8d6fe23d277d73b4926e25585c386282b5ad1bf6
jvid <judara2000@gmail.com>
2021-10-24 22:39:52 -0500

    Add post grid

---
+++ src/components/Post/Post.js
@@ -0,0 +1,63 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Link, useLocation } from 'react-router-dom'
+import Tag from '../ui/Tag/Tag'
+
+import HeartIcon from '../ui/Heart/HeartIcon'
+import User from '../ui/User/User'
+
+import './Post.css'
+
+const Post = ({ id, image, likes, tags, text, owner }) => {
+  const location = useLocation()
+  const fullName = `${owner.firstName} ${owner.lastName}`
+
+  return (
+    <article className="post">
+      <div className="post__user">
+        <User id={owner.id} name={fullName} picture={owner.picture} />
+      </div>
+      <div className="post__body">
+        <div className="post__img-container">
+          <img src={image} alt={text} loading="lazy" className="post__img" />
+        </div>
+        <div className="post__box">
+          <div className="post__info">
+            <div className="post__tags" aria-label="Post tags">
+              {tags.map((tag) => (
+                <Tag key={tag} name={tag} />
+              ))}
+            </div>
+            <div className="post__description">{text}</div>
+          </div>
+          <div className="post__actions-container">
+            <Link
+              to={{
+                pathname: `/post/${id}`,
+                state: { background: location },
+              }}
+              className="post__action"
+            >
+              Comments
+            </Link>
+            <Link to="#" className="post__action">
+              <HeartIcon className="post__action-icon post__action-icon--red" />
+              <span className="post__action-text">{likes}</span>
+            </Link>
+          </div>
+        </div>
+      </div>
+    </article>
+  )
+}
+
+Post.propTypes = {
+  id: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired,
+  likes: PropTypes.number.isRequired,
+  tags: PropTypes.array.isRequired,
+  text: PropTypes.string.isRequired,
+  owner: PropTypes.object.isRequired,
+}
+
+export default Post
---
+++ src/components/Posts/Posts.js
@@ -0,0 +1,29 @@
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+
+import { initPosts } from '../../store/post/actions'
+import Post from '../Post/Post'
+import PostLoader from '../ui/PostLoader/PostLoader'
+
+import './Posts.css'
+
+const Posts = () => {
+  const posts = useSelector(({ posts }) => posts)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    ;(() => {
+      dispatch(initPosts())
+    })()
+  }, [dispatch])
+
+  return (
+    <section className="posts">
+      {!posts.data
+        ? [...new Array(10)].map((_, i) => <PostLoader key={i} />)
+        : posts.data.map((post) => <Post key={post.id} {...post} />)}
+    </section>
+  )
+}
+
+export default Posts
